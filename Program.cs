4a62fa24ccee66ccf4a27e642bd867bad70a2232
prasadacharya <prasada@mail.usf.edu>
2020-01-22 21:43:42 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Even_or_Odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please provide your input number");
+
+            string A = Console.ReadLine();
+
+            int a = Convert.ToInt32(A);
+
+            if (a % 2 == 0)
+
+                Console.WriteLine("Number is even");
+
+            else
+
+                Console.WriteLine("Number is odd");
+        }
+    }
+}
